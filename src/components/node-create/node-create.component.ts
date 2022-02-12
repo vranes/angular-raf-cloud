4cb999979dd2eef74d11a860f074b7712a942a25
vranes <nikolinavranes@gmail.com>
2022-02-12 02:42:02 +0100

    implemented node-create service and component

---
+++ src/components/node-create/node-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-node-create',
+  templateUrl: './node-create.component.html',
+  styleUrls: ['./node-create.component.css']
+})
+export class NodeCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
